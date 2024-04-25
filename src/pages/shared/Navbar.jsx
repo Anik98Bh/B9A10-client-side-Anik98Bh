070406a72210b52361a3f340493a21f0b4c73dec
Anik98B <anikbhowmik0188@gmail.com>
2024-04-26 04:54:25 +0600

    added navbar routes

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,8 +1,36 @@
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+    const navLinks = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/allTourists">All Tourists Spot</NavLink></li>
+        <li><NavLink to="/addTourists">Add Tourists Spot</NavLink></li>
+        <li><NavLink to="/myList">My List</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/register">Register</NavLink></li>
+    </>
+
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className="navbar rounded fixed z-10 px-5 ">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLinks}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-3xl font-bold"><span className="text-green-500">AB</span>Tourism</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                   {navLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
         </div>
     );
 };
